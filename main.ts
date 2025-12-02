9550b414c424ea6426c82c37221f714ebd3b0bfc
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-12-02 15:26:22 +0000

    added notes

--- main.ts
+++ main.ts
@@ -5,12 +5,15 @@
  * @fileoverview created a program to tell you how many dollars and change you have
  */
 
+// variable
 let Money: number;
 Money = Number(prompt("Input the cents please:"));
 
+//constants
 const dollars = (Money / 100);
 const cents = (Money % 100);
 
+//output
 console.log(` that comes to ${dollars} dollars and ${cents} cents `)
 
 console.log("\nDone");
